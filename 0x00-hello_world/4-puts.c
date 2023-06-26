4c3037adb6f1d7fcfa9443e0ccf5694c8dbc258e
Salman <hussainyred@gmail.com>
2023-06-26 10:52:43 -0700

    c code to print out a message without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main - Entry Point
+ *
+ *Description : A C program that prints with puts function
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
