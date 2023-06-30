7baf4835ae7d86da6aaf06473ca2964cff9907e3
Salman <hussainyred@gmail.com>
2023-06-30 04:29:09 -0700

    assignment dos

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+	char str[] = "_putchar"
+	int i;
+	for (i=0;i<8; i++)
+		_putchar(str[i];
+	_putchar('\n');
+
+	return (0);
