df69067db59575549492ddf817e56794dd980763
Salman <hussainyred@gmail.com>
2023-06-30 06:14:16 -0700

    Whoops x2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print
+ * print_alphabet_x10 - utilizes on the _putchar function to print
  *                 the alphabet a - z 10 times
 */
 
@@ -9,7 +9,8 @@ void print_alphabet_x10(void)
 {
 	int loweralpha;
 	int printcounter;
-	for(printcounter = 0; printcounter < 10; printcounter++)
+
+	for (printcounter = 0; printcounter < 10; printcounter++)
 	{
 		for (loweralpha = 'a'; loweralpha <= 122; loweralpha++)
 			_putchar(loweralpha);
