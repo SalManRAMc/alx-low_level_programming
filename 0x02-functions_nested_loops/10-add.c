fb0cbde6bdd107bfd4d7dce424688ca3ce2dacb7
Salman <hussainyred@gmail.com>
2023-06-30 12:21:01 -0700

    ADD

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers and returns sum
+ *
+ * @a: input first number
+ * @b: input seconf operand
+ *
+ * Return: sum
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
