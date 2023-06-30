e87a0097ffca19b64692eb0b291861a173f1780a
Salman <hussainyred@gmail.com>
2023-06-30 10:56:47 -0700

    Times

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer- prints every minute and hour of the day
+ * from 00:00 to 23:59
+ *
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
