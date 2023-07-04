ef165948a96cb912eded6c35e92b39d055e434c8
Salman <hussainyred@gmail.com>
2023-07-04 07:02:23 -0700

    hotfix 2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory space s with a constant input till 
+ * *_memset - fills memory space s with a constant input till
  * the nth byte
  * @s: pointer to put constant in
  * @b: constant
@@ -14,9 +14,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
