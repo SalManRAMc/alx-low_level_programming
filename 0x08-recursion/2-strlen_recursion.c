c4dd1758349fd60e7952ffefe725fbe9909b01c7
Salman <hussainyred@gmail.com>
2023-07-06 14:15:57 -0700

    Let's gooo

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,19 +3,18 @@
 /**
  * _strlen_recursion - returns string length
  * @s: string
- * @i: number of chars, string length indicator
  * Return: length of string
 */
 
 int _strlen_recursion(char *s)
-{	
+{
 	int i;
 
-	i = 0;	
+	i = 0;
 	if (*s > '\0')
 	{
 		i = _strlen_recursion(s + 1);
-		i ++;
+		i++;
 		return (i);
 	}
 	return (i);
