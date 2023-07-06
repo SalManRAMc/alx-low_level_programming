91413239d239332a95f264142e027020373e2c5e
Salman <hussainyred@gmail.com>
2023-07-06 05:15:04 -0700

    completed recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * puts_recursion - prints a string followed by a new line
  *
  * @s: string to be printed
@@ -9,14 +8,18 @@
 
 void _puts_recursion_index(char *s, int i;)
 {
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion_index(s, i+1);
+	}
+	_putchar('\n');
+}
 
 void _puts_recursion(char *s)
 {
-	
 	int i;
-	while(s[i] != '\0')
-	{
-		_putchar (s[i]);
-		_puts_recursion(s[i]+1);
-	}
+	i = 0;
+
+	_puts_recursion_index(s, i);
 }
