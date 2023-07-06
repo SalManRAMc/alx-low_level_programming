32f0cf7c72e6557016ab1202562c37b95fefbe71
Salman <hussainyred@gmail.com>
2023-07-06 14:44:06 -0700

    First time's the charm, power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power function done through recursion
+ * @x: base integer
+ * @y: power integer
+ * Return: Value of X to the power of y, if y less than 0, return -1
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y-1));
+	else if (y == 0)
+		return (1);
+	else
+		return (-1);
+}
