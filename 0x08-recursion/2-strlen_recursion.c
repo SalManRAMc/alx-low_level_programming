be37461ead207e0333e65068f42d968a839bb2e6
SalManRAMc <hussainyred@gmail.com>
2023-08-03 02:01:54 -0700

    String length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - finds out string length through recursion
+ *
+ * @s: String input
+ *
+ * Return: Number of characters in string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		count += 1;
+	}
+	return (count);
+}
