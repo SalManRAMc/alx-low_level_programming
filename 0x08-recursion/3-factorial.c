79705d9b0fc0827876c07f7cd243aa480535dd46
SalManRAMc <hussainyred@gmail.com>
2023-08-03 02:25:38 -0700

    factorial function

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial product which is calculated recursively
+ *
+ * @n: integer whose product is to be calculated
+ *
+ * Return: Factorial Product
+ */
+
+int factorial(int n)
+{
+	int product = 1;
+
+	if (n > 0)
+	{
+		product = n * factorial(n - 1);
+		return (product);
+	}
+	else if (n < 0)
+		return (-1);
+	else
+	       return (1);	
+}
