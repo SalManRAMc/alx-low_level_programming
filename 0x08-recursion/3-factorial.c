6d9b70741b8fd06f89b8573d714b7a36007efcee
SalManRAMc <hussainyred@gmail.com>
2023-08-03 02:38:29 -0700

    Repairs

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -15,10 +15,10 @@ int factorial(int n)
 	if (n > 0)
 	{
 		product = n * factorial(n - 1);
-		return (product);
 	}
-	if (n < 0)
-		return -1;
-	if (n == 0)
-		return 1;
+	else if (n < 0)
+		product = -1;
+	else
+		product = 1 * factorial(n - 1);
+	return (product);
 }
