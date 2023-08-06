17671ba9c2f6fc410c50047ad19b717294e1dcbe
SalManRAMc <hussainyred@gmail.com>
2023-08-05 21:35:43 -0700

    Bettu debugs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- *  whatsmyname - the following program should print its' name 
+ *  main - the following program should print its' name
  *  upon compilation
  *
  *  @argc: argument count
  *  @argv: array containing pointers to arguments passed
  *
- *  Return : 1 on success
+ *  Return: 1 on success
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (1);
 }
