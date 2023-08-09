35f6aad0854a02fc78c093251ff0e675e2864526
SalManRAMc <hussainyred@gmail.com>
2023-08-09 09:25:24 -0700

    Hotfix 22

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 
 	n = strlen(str);
 
-	if (*str == NULL)
+	if (str == NULL)
 	{
 		printf("input string is empty");
 		return (NULL);
@@ -35,7 +35,5 @@ char *_strdup(char *str)
 		copiedarray[i] = str[i];
 		i++;
 	}
-	i++;
-	copiedarray[i] = str[i];
 	return (copiedarray);
 }
