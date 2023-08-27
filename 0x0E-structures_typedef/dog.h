9d0808bf8db0e7f2781dc84e11714b406f098367
SalManRAMc <hussainyred@gmail.com>
2023-08-27 02:41:39 -0700

    compadre

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,21 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - second dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: Just a lone dog struct in a big kitty world
+ */
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
