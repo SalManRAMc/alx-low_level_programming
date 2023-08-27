b31f0986bd732844bdada255600cefd78ada8c6d
SalManRAMc <hussainyred@gmail.com>
2023-08-26 20:34:14 -0700

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+The purpose of this directory is to highlight the tasks pertaining our understanding of typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name (char)
+ * @age: dog's age (int)
+ * @owner: owner's name {char}
+ *
+ * Description: just a lobe dog struct in a big kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
