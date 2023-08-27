b0c9ba72dea27ebe6b400745c95693f6033096fb
SalManRAMc <hussainyred@gmail.com>
2023-08-27 05:56:21 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *newarray;
 	unsigned int counter1, counter2, len2 = _strlen(s2);
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (n >= len2)
 		n = len2;
 
