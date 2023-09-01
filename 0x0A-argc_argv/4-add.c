c4ae296e5c8361ec90d3c29336ee18e7c92598ff
Salman <hussainyred.com>
2023-09-01 12:52:29 -0700

    objective error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,11 @@ int main(int argc, char **argv)
 {
 	int i, sum = 0;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		if (*argv[i] < '0' || *argv[i] > '9')
