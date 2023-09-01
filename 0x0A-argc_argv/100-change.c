12c7d93e95e92d306db7b9f930f1ce64176ba95c
Salman <hussainyred.com>
2023-09-01 13:04:04 -0700

    hotfix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,29 +12,27 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int i, remainder = atoi(argv[1]), change = 0;
-		int cents[] = {25, 10, 5 ,2 , 1};
+	int i, remainder = atoi(argv[1]), change = 0;
+	int cents[] = {25, 10, 5 ,2 , 1};
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
 		for (i = 0; i < 5; i++)
 		{
 			if (remainder >= cents[i])
 			{
 				change += remainder / cents[i];
 				remainder = remainder % cents[i];
-				if (money % cents[i] == 0)
-				{
-				break;
-				}
+				if (remainder == 0)
+					break;
 			}
 		}
 	printf("%d\n", change);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
