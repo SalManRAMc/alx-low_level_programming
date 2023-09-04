4aa0eccb6ddf8b5b8fefeec1e6b0ef3880368a0d
Salman ElHussini <37924900+SalManRAMc@users.noreply.github.com>
2023-09-04 20:51:05 +0300

    From Pc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,28 +14,27 @@
 
 char *_strdup(char *str)
 {
-  int len = 0, i;
-  char *newstrcpy;
+	int len = 0, i;
+	char *newstrcpy;
 
-  if (str == NULL)
-    return (NULL);
+	if (str == NULL)
+	return (NULL);
 
-  for (i = 0; str[i] != '\0'; i++)
-    len++;
+	for (i = 0; str[i] != '\0'; i++)
+	len++;
 
-  newstrcpy = malloc((len + 1) * sizeof(char));
+	newstrcpy = malloc((len + 1) * sizeof(char));
 
-  if (newstrcpy == NULL)
-    {
-      printf("Failed to allocate");
-      return (NULL);
-    }
-  else
-    {
-      for (i = 0; i < len; i++)
-	newstrcpy[i] = str[i];
-      
+	if (newstrcpy == NULL)
+	{
+	printf("Failed to allocate");
+	return (NULL);
+	}
+	else
+	{
+	for (i = 0; i < len; i++)
+		newstrcpy[i] = str[i];
       newstrcpy[i + 1] = '\0';
-    }
+}
   return (newstrcpy);
 }
