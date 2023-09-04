599ee97bcbeaf4cf518942cb4437b6dfbe678153
Salman <hussainyred.com>
2023-09-03 22:05:59 -0700

    First task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of characters and initializes it
+ * with a specfic character
+ *
+ *@size: size of array
+ *@c: character used to fill array
+ *
+ *Return: NULL if size is 0, otherwise pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+  char *ar;
+
+  if (size == 0)
+    return (NULL);
+  
+  ar = malloc(size * sizeof(char));
+  if (ar == NULL)
+    return (NULL)
+    else
+      {
+	for (; ar < size; ar++)
+	  *ip = c;
+	return (ar);
+      }
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+The purpose of this task is to learn and implement malloc
+as well as dynamic allocation as a concept.
