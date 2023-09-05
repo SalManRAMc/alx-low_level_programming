e877456a24454947151e8bf19429fe000fb3a11c
Salman ElHussini <37924900+SalManRAMc@users.noreply.github.com>
2023-09-05 17:35:30 +0300

    test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		mainarr[i] = malloc(width * sizeof(int));
+		mainarr[i] = malloc(height * sizeof(int));
 		if (mainarr[i] == NULL)
 		{
 			printf("Memory Allocation Failed");
