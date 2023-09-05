30a02fffa430611e245dd8f1bf4559a193660b8a
Salman ElHussini <37924900+SalManRAMc@users.noreply.github.com>
2023-09-05 16:06:42 +0300

    more debugs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,9 @@ char *str_concat(char *s1, char *s2)
 	char *newstring;
 
 	if (s1 == NULL)
-		*s1 = "";
+		*s1 = '\0';
 	if (s2 == NULL)
-		*s2 = "";
+		*s2 = '\0';
 
 	for (counter = 0; s1[counter] != '\0'; counter++)
 		len1++;
