0bb2b56e3d1ea8712e4c670d35e147f5172ce50c
Salman ElHussini <37924900+SalManRAMc@users.noreply.github.com>
2023-09-05 16:56:46 +0300

    Test

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *
+ * **alloc_grid - make a two dimensional array, initialised with zeros
+ *
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: Null on failure, pointer to grid on success
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *mainarr, i, j;
+
+	mainarr = malloc(width * sizeof(int));
+	if (mainarr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		mainarr[i] = malloc(height * sizeof(int));
+		if (mainarr[i] == NULL)
+		{
+			printf("Memory Allocation Failed");
+			return (NULL);
+		}
+	}
+	
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+			mainarr[i][j] = 0;
+	return (mainarr);
+}
