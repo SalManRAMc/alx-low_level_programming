259b77d02c7e812052ee4dcdd5c9a411fbe9877d
Salman ElHussini <37924900+SalManRAMc@users.noreply.github.com>
2023-09-05 17:27:43 +0300

    test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 	if (mainarr == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		mainarr[i] = malloc(width * sizeof(int));
 		if (mainarr[i] == NULL)
