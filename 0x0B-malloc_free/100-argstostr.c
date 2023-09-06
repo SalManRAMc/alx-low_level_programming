d2e37c1984338dca3d8a27f91158707f8cd27d49
Salman ElHussini <37924900+SalManRAMc@users.noreply.github.com>
2023-09-06 12:07:53 +0300

    additional files

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *argstostr - concatenates arguments into one string
+ *
+ * @ac: argcount
+ * @av: array of strings (array of arguments)
+ *
+ * Return: Pointer to big string, or NULL on failure
+ */
+
+char *argstostr(int ac, char **av)
+{
+}
