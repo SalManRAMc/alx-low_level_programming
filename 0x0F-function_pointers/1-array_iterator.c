1552f10c4ff93a814f34b3316f5a9e3cbb9ddca1
Salman ElHussini <hussainyred@gmail.com>
2023-09-15 16:17:41 +0300

    lots of wrapping

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * array_iterator - calls upon a function that goes through an array 
+ *
+ * @array: array of ints being used
+ * @size: size of array
+ * @action: pointer to function that itirates through array
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
 #endif
