299fc6a11be581e537e85b5accb63566fdc824a2
Salman ElHussini <hussainyred@gmail.com>
2023-09-29 18:07:30 +0300

    wjoop

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint - converts binary to int
  *
- *@b: binary number
+ * @b: binary number
  *
  * Return: converted number or 0 on fail
  */
@@ -14,27 +14,31 @@ unsigned int binary_to_uint(const char *b)
 	int base = 1, strlen = 0, index;
 
 
-	/*get to last index in string (because binary nums are read from right to
-	 left*/
+	/*
+	 * get to last index in string (because binary nums are read from right to
+	 * left
+	 */
 	while (b[strlen])
 	{
 		strlen++;
 	}
 
-	/*binary is read from right to left, 
-	  hence why we start at last index before '\0' */
+	/*
+	 * binary is read from right to left,
+	 * hence why we start at last index before '\0'
+	 */
 	for (index = strlen - 1; index >= 0; index--)
 	{
-		/*checks if number has anything other than 0s or 1s*/
-		if(b[index] != '0' && b[index] != '1')
-			return 0;
-		/*process of checking if indexed space is 1*/
+		/* checks if number has anything other than 0s or 1s */
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
+		/* process of checking if indexed space is 1 */
 		if (b[index] == '1')
 		{
-			/*if indexed space is 1, add binary equivalent to decimal result*/
-			decimal += (1*base);
+		/* if indexed space is 1, add binary equivalent to decimal result */
+			decimal += (1 * base);
 		}
-		/*move on to next base 2 number*/
+		/* move on to next base 2 number */
 		base *= 2;
 	}
 	return (decimal);
