2d8f5497008665d98cb11540b2442797120a6236
Salman ElHussini <hussainyred@gmail.com>
2023-09-29 16:53:20 +0300

    polo

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ *
+ *@b: binary number
+ *
+ * Return: converted number or 0 on fail
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int index, decimal, base = 1;
+
+
+	/*get to last index in string (because binary nums are read from right to
+	 left*/
+	while (b[index])
+	{
+		index++;
+	}
+
+	/*binary is read from right to left, 
+	  hence why we start at last index before '\0' */
+	for (index; index >= 0; index--)
+	{
+		/*checks if number has anything other than 0s or 1s*/
+		if(b[index] != '0' || b[index] != '1')
+			return 0;
+		/*process of checking if indexed space is 1*/
+		if (b[index] == '1')
+		{
+			/*if indexed space is 1, add binary equivalent to decimal result*/
+			decimal += (1*base);
+		}
+		/*move on to next base 2 number*/
+		base *= 2;
+	}
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int c);
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+#endif
