82b63ed0cd1a1c1a7488052b260cfecba3c46278
Salman ElHussini <hussainyred@gmail.com>
2023-09-29 17:16:43 +0300

    polo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,22 +10,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int index, decimal, base = 1;
+	unsigned int decimal = 0;
+	int base = 1, strlen = 0, index;
 
 
 	/*get to last index in string (because binary nums are read from right to
 	 left*/
-	while (b[index])
+	while (b[strlen])
 	{
-		index++;
+		strlen++;
 	}
 
 	/*binary is read from right to left, 
 	  hence why we start at last index before '\0' */
-	for (index; index >= 0; index--)
+	for (index = strlen - 1; index >= 0; index--)
 	{
 		/*checks if number has anything other than 0s or 1s*/
-		if(b[index] != '0' || b[index] != '1')
+		if(b[index] != '0' && b[index] != '1')
 			return 0;
 		/*process of checking if indexed space is 1*/
 		if (b[index] == '1')
