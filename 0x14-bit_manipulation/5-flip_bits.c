57aa28541804dfedc7328b05999fa760bcb2ed68
Salman ElHussini <hussainyred@gmail.com>
2023-09-30 13:15:12 +0300

    minor bug

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *  * flip_bits - return number of bits that would need to be flipped to
- *   * transform one number to another
- *    *
- *     * @n: first number
- *      * @m: second number
- *       *
- *        * Return: number of bits to flip to convert numbers
- *         */
+ * flip_bits - return number of bits that would need to be flipped to
+ * transform one number to another
+ *
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to flip to convert numbers
+ */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xorval = n ^ m;
@@ -20,4 +21,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 				count++;
 			xorval = xorval >> 1;
 		}
-	re}
+	return (count;)
+}
