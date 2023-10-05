1e2c7a7aa73e14375390b6c8ebe961102e3606bd
Salman ElHussini <hussainyred@gmail.com>
2023-10-05 17:05:53 +0300

    polo1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of nodes in a list
+ *
+ *@h: head of list
+ *
+ *Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodenumber;
+	list_t *header;
+
+	header = (list_t *)h;
+
+	while (header->next == NULL)
+		nodenumber++;
+
+	return (nodenumber);
+}
