9922592cae28518e54502fdaf89b1b7b8119ea70
Salman ElHussini <hussainyred@gmail.com>
2023-10-05 18:49:13 +0300

    im as cool as ali 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,7 +4,7 @@
  * add_node - adds a new node to list
  *
  * @head: pointer to current head of list
- *  
+ *
  *  head of list is dynamically allocated, hence it's a ptr
  *  hence head is a pointer to a pointer.
  *  key-concept: dynamic allocation
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	
+
 	if (new_node == NULL)
 	{
 		printf("creation of new node failed");
@@ -27,7 +27,8 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new_node->next = *head; /*as if to say that new_node points at the next node*/
 	new_node->len = strlen(str); /*value of strlen*/
-	new_node->str = strndup(str, strlen(str)); /*use strndup as it's a safer duplication method, strlen is passed*/
+	new_node->str = strndup(str, strlen(str));
+	/*use strndup as it's a safer duplication method, strlen is passed*/
 
 	*head = new_node; /*head now points at the new node*/
 	return (*head);
