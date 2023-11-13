450b95fa58739ff905c2f6d46f60bdd50c39d596
Salman ElHussini <hussainyred@gmail.com>
2023-11-13 19:20:19 +0200

    polo2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,7 +12,7 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *navigator; /*pointer to navigate through list*/
-	int count = 0
+	int count = 0;
 
 	if (h == NULL) /*if list is empty, return fail value*/
 		return (0);
