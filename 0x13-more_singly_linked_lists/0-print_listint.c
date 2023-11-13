2f742febbbb2c6775641787aa92b7b893d8f3e6a
Salman ElHussini <hussainyred@gmail.com>
2023-11-13 18:43:47 +0200

    pol

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a list of typr int
+ *
+ * @h: head node 
+ *
+ * Return 1 on success, -1 on failure
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t *navigator; /*pointer to navigate through list*/
+
+	if (h == NULL) /*if list is empty, return fail value*/
+		return -1;
+
+	navigator = h; /*points to head node*/
+
+	if (navigator != NULL)
+	{
+		print("%d", navigator->int);
+		navigator = navigator->next;
+		/*print node data then move to next*/
+	}
+	return 1;
+}
