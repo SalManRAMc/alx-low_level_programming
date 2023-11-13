03a8afd515d6c3834e7d280f9807a0dfcef0061a
Salman ElHussini <hussainyred@gmail.com>
2023-11-13 20:36:28 +0200

    pop4

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+*
+* listint_len - returns the number nodes in a list
+*
+* @h: header node
+*
+* Return: Number of Nodes available
+*
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int length = 0;
+	const listint_t *navigator;
+
+	if (h == NULL) /*checks if the head pointer points to NULL*/
+		return (0);
+
+	navigator = h; /*give navigator ptr address of first node*/
+
+	while(navigator->next != NULL)
+	{
+		length++;
+		navigator = navigator->next;
+	}
+	return(length);
+}
