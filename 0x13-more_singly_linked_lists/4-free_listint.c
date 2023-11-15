c2c110b01193d99d650b1dfecb26cfc7859fb159
Salman ElHussini <hussainyred@gmail.com>
2023-11-15 20:54:44 +0200

    woooooohoooooooo

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h" 
+
+/**
+ * free_listint - frees list 
+ *
+ * @head: head node
+ *
+ * Return nothing, tis void
+*/
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *temporarynode, *navigator;
+
+	if (head == NULL)
+		return;
+
+	navigator = head;
+
+	while(navigator != NULL)
+	{
+		temporarynode = navigator->next;
+		free(navigator);
+		navigator = temporarynode;
+	}
+	return;
+}
