3f0539d60c0ab5e5987c8b1157f2249355e2d3c5
SalmanRAMc <hussainyred@gmail.com>
2024-02-06 13:21:53 +0300

    polo

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,22 +14,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index;
 	hash_node_t *newhnode;
+	unsigned long int hash;
 
 	newhnode->key = key;
 	newhnode->value = value;
 	newhnode->next = NULL;
+	hash = hash_djb2(key);
 
-	for(index = 0; ht->array[index] != NULL; index++)
-	{
-		if (ht->array[index] == newhashnode->key)
+	for (index = 0; ht->array[index] != NULL; index++)
+
+		if (ht->array[index]->key == newhashnode->key)
 		{
 			if (ht->array[index] == NULL)
 			{
-				ht->array[index++] = newhashnode;
+				ht->array[index] = newhashnode;
 				return (1);
 			}
 			else
-				return (0);
+			{
+				ht->array[index]->next = newhashnode;
+			}
 		}
 	}
 	return (0);
